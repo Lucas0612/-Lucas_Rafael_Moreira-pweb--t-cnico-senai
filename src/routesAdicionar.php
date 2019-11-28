5bc5aac35f174e2c9bc720b15e3bdd9ba7433a3d
Lucas0612 <51293565+Lucas0612@users.noreply.github.com>
2019-11-28 17:29:18 -0200

    Não Deu

--- src/routesAdicionar.php
+++ src/routesAdicionar.php
@@ -7,27 +7,27 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
-    // $app->get('/adicionar/', function (Request $request, Response $response, array $args) use ($container) {
+    $app->get('/adicionar/', function (Request $request, Response $response, array $args) use ($container) {
 
-    //     // Sample log message
-    //     $container->get('logger')->info("Slim-Skeleton '/adicionar/' route");
+        // Sample log message
+        $container->get('logger')->info("Slim-Skeleton '/adicionar/' route");
 
-    //     return $container->get('renderer')->render($response, 'inicio.phtml', $args);
+        return $container->get('renderer')->render($response, 'adicionar.phtml', $args);
 
 
-    // });
+    });
 
     $app->post('/adicionar/', function (Request $request, Response $response, array $args) use ($container) {
 
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/adicionar/' route");
 
-        $conexao =$container['pdo'];
+        $conexao = $container['pdo'];
 
         
         $adicionar = $_POST;
 
-        $resultSet = $conexao->query('INSERT INTO carro SELECT('.$adicionar['id'].', '.$adicionar['modelo'].', '.$adicionar['marca'].', '.$adicionar['ano'].')')->fetchAll();
+        $resultSet = $conexao->query('INSERT INTO carro WHERE('.$adicionar['id'].', '.$adicionar['modelo'].', '.$adicionar['marca'].', '.$adicionar['ano'].')')->fetchAll();
 
         
 
@@ -35,7 +35,7 @@ return function (App $app) {
 
 
         // Render index view
-        return $container->get('renderer')->render($response, 'inicio.phtml', $args);
+        return $container->get('renderer')->render($response, 'adicionar.phtml', $args);
 
 
     });
